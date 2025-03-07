64e66cf7cd53750ab7e8762374c808f00b1a4fd0
Leonardo Gomes <leonardodasigomes@gmail.com>
2025-03-07 15:35:23 -0300

    🐛 Adjust bug of categories

--- scripts/create_post.sh
+++ scripts/create_post.sh
@@ -9,6 +9,5 @@ echo "title: $GITHUB_ISSUE_TITLE" >> ${file_path}
 echo "date: $GITHUB_ISSUE_CREATED_AT" >> ${file_path}
 echo "author: $GITHUB_ISSUE_USER" >> ${file_path}
 # echo "url: $GITHUB_ISSUE_URL" >> ${file_path} 
-echo "---" >> ${file_path}
 echo "$GITHUB_ISSUE_BODY" >> ${file_path}
 
