23c77920da9604dbbe61413de3fef2d8fc9bf6d5
Leonardo Gomes <leonardodasigomes@gmail.com>
2025-03-10 15:50:06 -0300

    ✨ Add verification to edit file

--- scripts/update_user_info.sh
+++ scripts/update_user_info.sh
@@ -2,4 +2,8 @@ file_path="user_info.yml"
 
 echo "$GITHUB_ISSUE_BODY" >> ${file_path}
 
+# Verificar se o arquivo YAML está corretamente formatado
+python3 -c "import yaml; yaml.safe_load(open('${file_path}'))" || { echo 'Erro ao carregar o arquivo YAML'; exit 1; }
+
+# Executar o script Python para processar o arquivo YAML
 python3 scripts/process_user_info.py
